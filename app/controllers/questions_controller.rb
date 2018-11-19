7c218366aca667973edc7d43a2c4c78c42ff8d8d
william segard <william.segard@gmail.com>
2018-11-19 12:45:50 +0100

    stupid coaching

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    if @question == 'I am going to work'
+      return @answer = 'Great!'
+    elsif @question.chars.last(1).join == '?'
+      return @answer = 'Silly question, get dressed and go to work!'
+    else
+      return @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
